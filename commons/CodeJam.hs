9b4e1ea504d74b50f935984b9189eefc3123702b
Dog <lgmsantos@gmail.com>
2013-02-25 10:01:17 -0300

    CodeJam commons

---
+++ commons/CodeJam.hs
@@ -0,0 +1,32 @@
+module CodeJam(Problem(..), takeS, takeSN)
+where
+
+import Data.List
+import Text.Printf
+import Control.Monad.State
+
+takeS :: Int -> State [String] [String]
+takeS n = do
+    state <- get
+    put $ drop n state
+    return $ take n state
+
+takeSN :: State [String] [String]
+takeSN = do
+    (n:state) <- get
+    put state
+    takeS (read n)
+
+class Problem a where
+    readProblem :: [String] -> Maybe (a, [String])
+    showSolution :: a -> String
+
+    readProblems :: [String] -> [a]
+    readProblems (count:content) = unfoldr readProblem content
+
+    loadProblems :: FilePath -> IO [a]
+    loadProblems = (return.readProblems.words =<<) . readFile
+
+    printProblems :: [a] -> IO ()
+    printProblems problems = mapM_ putStrLn $ zipWith printProblem ([1..]::[Int]) problems
+        where printProblem i p = printf "Case #%d: %s" i (showSolution p)
